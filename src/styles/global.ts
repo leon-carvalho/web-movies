3ed1fed4ba62b074f6078a95f2ad8039342ee55d
leon-carvalho <leonardoscc15@gmail.com>
2020-11-06 23:04:55 -0300

    refactor: set colors and font-family in global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,8 +7,26 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    --primary: #116193;
+    --secondary: #04BFCB;
+    --background: #FFFFFF;
+    --text: #787878;
+    --bright: #EBEBEB;
+  }
+
   body {
-    color: #fff;
-    background: #333;
+    color: var(--text);
+    background: var(--background);
+  }
+
+  body, input, button, textarea {
+    font-weight: 300;
+    font-family: 'Lato', sans-serif;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-weight: 400;
+    font-family: 'Abel', sans-serif;
   }
 `
