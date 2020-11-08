21b576b49aa8fece5045d7a1dbf8dcf338d60413
leon-carvalho <leonardoscc15@gmail.com>
2020-11-08 12:05:18 -0300

    style: add and style searchbar component

--- src/styles/components/searchbar.ts
+++ src/styles/components/searchbar.ts
@@ -1,6 +1,29 @@
-import styled from 'styled-components'
+import { InputHTMLAttributes } from 'react'
+import styled, { keyframes, css } from 'styled-components'
 
-export const SearchBar = styled.input`
+interface ISearchBarProps extends InputHTMLAttributes<HTMLInputElement> {
+  isErrored?: boolean
+}
+
+const shakeAnimation = keyframes`
+   10%, 90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%, 80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%, 50%, 70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%, 60% {
+    transform: translate3d(4px, 0, 0);
+  }
+`
+
+export const SearchBar = styled.input<ISearchBarProps>`
   width: 100%;
   height: 56px;
   border: none;
@@ -19,6 +42,14 @@ export const SearchBar = styled.input`
   &:focus {
     border: 2px solid var(--primary);
   }
+
+  ${({ isErrored }) =>
+    isErrored &&
+    css`
+      background-color: #ffebee;
+      border: 2px solid var(--danger) !important;
+      animation: ${shakeAnimation} 0.6s both linear;
+    `}
 
   @media (max-width: 432px) {
     font-size: 14px;
