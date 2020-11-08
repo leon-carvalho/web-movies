d795a513c4aa840c1a7035d82945cef6a808eb41
leon-carvalho <leonardoscc15@gmail.com>
2020-11-08 12:52:50 -0300

    feat: convert minutes to hours and minutes

---
+++ src/utils/convertMinutesToHours.ts
@@ -0,0 +1,8 @@
+const convertMinutesToHourAndMinutes = (totalMinutes: number) => {
+  const hours = Math.floor(totalMinutes / 60)
+  const minutes = totalMinutes % 60
+
+  return `${hours}h ${minutes}min`
+}
+
+export { convertMinutesToHourAndMinutes }
